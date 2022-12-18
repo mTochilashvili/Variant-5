87f19fc1b5f584dad057185fe9b8c27909cd1af4
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-12-18 12:47:08 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Variant5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(740, 345);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(740, 378);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 27);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(740, 411);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 27);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(704, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(703, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(704, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x =";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 47);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SinA",
+            "CosA",
+            "TanA",
+            "CtanA"});
+            this.comboBox1.Location = new System.Drawing.Point(531, 352);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Variant5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == ""))
+            {
+                MessageBox.Show("Пожалуйста убедитесь в правильности ввода данных и повторите попытку");
+            }
+            else
+            {
+                int x = Convert.ToInt32(textBox3.Text);
+                int imax = 400;
+                int period = 2; 
+                int a = Convert.ToInt32(textBox1.Text); 
+                int x0 = 20; 
+                int y0 = a;
+                double[] f = new double[imax * period + 10];
+                double b = Convert.ToDouble(textBox2.Text);
+                IStrategy s = new SinA();
+                if (comboBox1.Text == "SinA")
+                    s = new SinA();
+                if (comboBox1.Text == "CosA")
+                    s = new CosA();
+                if (comboBox1.Text == "TanA")
+                    s = new TanA();
+                if (comboBox1.Text == "CtanA")
+                    s = new CtngA();
+
+                // Функция
+                for (int i = 0; i < imax * period; i++)
+                {
+                    f[i] = s.Function(a, b, i, imax, x);
+                }
+
+                Graphics g = Graphics.FromHwnd(this.Handle);
+                Pen pen = Pens.Black;
+
+                g.DrawLine(pen, x0, y0, x0 + imax * period, y0); //Рисуем ось X
+                g.DrawLine(pen, x0, y0 - a, x0, y0 + a); //Рисуем ось Y
+
+                for (int i = 0; i < imax * period; i++) //Рисуем график
+                {
+                    int y1 = y0 - (int)f[i]; //Координата Y[i]
+                    int y2 = y0 - (int)f[i + 1]; //Координата Y[i+1]
+                    g.DrawLine(pen, x0 + i, y1, x0 + i + 1, y2);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        public interface IStrategy
+        {
+            public double Function(int a, double b, int i, int imax, int x);
+        }
+        public class SinA : IStrategy
+        {
+            public double Function(int a, double b, int i, int imax, int x)
+            {
+                return Math.Round(a * Math.Sin(b * (x * Math.PI / imax * i)));
+            }
+        }
+        public class CosA : IStrategy
+        {
+            public double Function(int a, double b, int i, int imax, int x)
+            {
+                return Math.Round(a * Math.Cos(b * (x * Math.PI / imax * i)));
+            }
+        }
+        public class TanA : IStrategy
+        {
+            public double Function(int a, double b, int i, int imax, int x)
+            {
+                double tgA = Math.Tan(b * (x * (Math.PI / imax * i)));
+                if ((tgA < Math.PI / 2) && (tgA > -1 * (Math.PI / 2)))
+                    return Math.Round(a * tgA);
+                else 
+                    return 0;
+            }
+        }
+        public class CtngA : IStrategy
+        {
+            public double Function(int a, double b, int i, int imax, int x)
+            {
+                double ctgA = 1 / Math.Tan(b * (x * (Math.PI / imax * i)));
+                if ((ctgA < Math.PI / 2) && (ctgA > -1 * (Math.PI / 2)))
+                    return Math.Round(a * ctgA);
+                else
+                    return 0;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
